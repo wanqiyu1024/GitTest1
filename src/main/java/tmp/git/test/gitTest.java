6d4c8d5f744ae4e7957d1ecd85e82cbac071dbdc
wan <1981773689@qq.com>
2020-04-15 18:39:26 +0800

    gitTest

--- src/main/java/tmp/git/test/gitTest.java
+++ src/main/java/tmp/git/test/gitTest.java
@@ -2,6 +2,6 @@ package tmp.git.test;
 
 public class gitTest {
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("hello world!!!");
     }
 }
